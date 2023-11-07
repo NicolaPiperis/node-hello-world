1a38513446270d516833e62009a555a40cbfb12f
NicolaPiperis <nicolapiperis9999@gmail.com>
2023-11-07 16:55:35 +0100

    Inizio Bonus

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("Hello world!");
+console.log(process.argv[2]);
+
+// typeof definisce la natura dell'elemento
+console.log(typeof "Hello world!");
+
+
--- server.js
+++ server.js
@@ -1,3 +1,49 @@
 const http = require ("http");
 
+const dotenv = require ("dotenv");
+dotenv.config();
+// forma contratta
+// require("dotenv").config();
+
 const port = process.env.PORT || 3000;
+
+const frasiIspirazionali = [ 
+    "La vita è per il 10% cosa ti accade e per il 90% come reagisci. (Charles R. Swindoll)",
+    "Ci sono due regole nella vita: 1. Non mollare mai; 2. Non dimenticare mai la regola n° 1. (Duke Ellington)",
+    "Se qualcosa non ti piace, cambiala. Se non puoi cambiarla, cambia il tuo atteggiamento. Non lamentarti. (Maya Angelou)",
+    "Se vuoi qualcosa che non hai mai avuto, devi fare qualcosa che non hai mai fatto. (Thomas Jefferson)",
+    "Non essere una 'se-potessi-sarei o una se-volessi-potrei-essere'. Sii e basta. (Sarah Ban Breathnach)",
+    "Il mio scopo nella vita? Fare solo cose positive. (Zendaya)",
+    "Prima ti ignorano, poi ti deridono, poi ti combattono. Poi vinci. (Mahatma Gandhi)",
+    ""
+]
+
+// funzione per scrivere in html
+function htmlResponse(res, content) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(content);
+}
+
+
+// creo il server
+const server = http.createServer(function (req, res){
+
+    // dico cosa deve fare, in questo caso gli dico che deve rispondere con una stringa oppure manipolando i tag html come in questo caso
+    // res.end(`
+    // <h1>Il server funziona!</h1>
+    // Il codice &egrave; ${process.env.CODICE} 
+    // `);
+
+    // oppure direttamente tramite funzione
+    htmlResponse(res, `<h1>Il server funziona!</h1>
+    Il codice &egrave; ${process.env.CODICE} `
+    );
+
+
+});
+
+// avvio il serverg
+server.listen(port,function(){
+
+    console.log("Il server si trova sulla porta:" + port)
+});
